443c33a9cc9b518835a6f43d3dfd901b79b7b614
omkarhole <omkarhole314@gmail.com>
2025-10-12 02:25:31 +0530

    created login setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,44 @@
-export const signup=(req,res)=>{
-    res.send("signup  route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const signup=async(req,res)=>{
+   const {fullName,email,password}=req.body;
+    try{
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be at least 6 characters long"});
+        }
+        const user=await User.findOne({email})
+        if(user) return res.status(400).json({message: "User already exists with this email"});
+
+        const salt=await bcrypt.genSalt(10);
+        // generate hash password with help of salt
+        const hashedPassword=await bcrypt.hash(password,salt)
+        // create new user
+        const newUser=new User({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+        if(newUser){
+        generateToken(newUser.id,res)
+        await newUser.save();
+        res.status(201).json({
+            _id:newUser.id,
+            fullName:newUser.fullName,
+            email:newUser.email,
+            profilePic:newUser.profilePic,
+            message:"User created successfully"
+        })
+        }
+    }
+    catch(err){
+        console.log("error in signup controller",err);
+        res.status(500).json({message:"Internal server error"});
+    }
 }
 
 export const login=(req,res)=>{
