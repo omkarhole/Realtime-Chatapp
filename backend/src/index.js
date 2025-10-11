63aeba5a8abe2fd894f6059f4762dcc706b2cdb9
omkarhole <omkarhole314@gmail.com>
2025-10-12 01:12:27 +0530

    added basic database setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
- 
-const app=  express();
-import authRoutes from "./routes/auth.route.js"
+ import dotenv from "dotenv";
+const app=express();
 
+import {connectDB} from "./lib/db.js"
+import authRoutes from "./routes/auth.route.js"
+dotenv.config()
 app.use(express.json());
 
+const PORT=process.env.PORT
 
 
 
@@ -15,6 +18,7 @@ app.get("/",(req,res)=>{
     res.send("hello from backend");
 })
 
-app.listen(5001,()=>{
-    console.log("server is running on port 5001");
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+    connectDB()
 })
