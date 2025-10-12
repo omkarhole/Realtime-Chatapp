ef13bf559fc0fdc091427a34aaf3a0f95e23c090
omkarhole <omkarhole314@gmail.com>
2025-10-12 12:01:27 +0530

    added login logout functionality with middleware which checks whether is login or not

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,50 +1,97 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const signup=async(req,res)=>{
-   const {fullName,email,password}=req.body;
-    try{
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
 
-        if(!fullName || !email || !password){
-            return res.status(400).json({message:"All fields are required"});
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
         }
-        if(password.length<6){
-            return res.status(400).json({message:"Password must be at least 6 characters long"});
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
         }
-        const user=await User.findOne({email})
-        if(user) return res.status(400).json({message: "User already exists with this email"});
+        const user = await User.findOne({ email })
+        if (user) return res.status(400).json({ message: "User already exists with this email" });
 
-        const salt=await bcrypt.genSalt(10);
+        const salt = await bcrypt.genSalt(10);
         // generate hash password with help of salt
-        const hashedPassword=await bcrypt.hash(password,salt)
+        const hashedPassword = await bcrypt.hash(password, salt)
         // create new user
-        const newUser=new User({
+        const newUser = new User({
             fullName,
             email,
             password: hashedPassword
         })
-        if(newUser){
-        generateToken(newUser.id,res)
-        await newUser.save();
-        res.status(201).json({
-            _id:newUser.id,
-            fullName:newUser.fullName,
-            email:newUser.email,
-            profilePic:newUser.profilePic,
-            message:"User created successfully"
-        })
+        if (newUser) {
+            generateToken(newUser.id, res)
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser.id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+                message: "User created successfully"
+            })
         }
     }
-    catch(err){
-        console.log("error in signup controller",err);
-        res.status(500).json({message:"Internal server error"});
+    catch (err) {
+        console.log("error in signup controller", err);
+        res.status(500).json({ message: "Internal server error" });
     }
 }
 
-export const login=(req,res)=>{
-    res.send("login route");
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: "invalid Credentials" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ message: "invalid Credentials" });
+        }
+
+        generateToken(user.id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+            message: "Login successful"
+
+        })
+
+    }
+    catch (err) {
+        console.log("error in login controller", err);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
-export const logout=(req,res)=>{
-    res.send("logout route");
+export const logout = async (req, res) => {
+   try{
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({ message: "Logout successful" });
+   }
+   catch(err){
+       console.log("error in logout controller", err);
+       res.status(500).json({ message: "Internal server error" });
+   }
+}
+
+export const updateProfile=async(req,res)=>{
+    const {profilePic}=req.body;
+    try{
+        
+    }
+    catch(err){
+        console.log("error in update profile controller", err);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
  import dotenv from "dotenv";
-const app=express();
+ import cookieParser from "cookie-parser";
+ 
+ import {connectDB} from "./lib/db.js"
+ import authRoutes from "./routes/auth.route.js"
 
-import {connectDB} from "./lib/db.js"
-import authRoutes from "./routes/auth.route.js"
-dotenv.config()
-app.use(express.json());
+ dotenv.config()
+ const app=express();
+ 
+ app.use(express.json());
+ app.use(cookieParser());
 
 const PORT=process.env.PORT
 
