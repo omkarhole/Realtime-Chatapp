c5d855930b7c598e54564cb97945e8c403a5b556
souma9830 <prabirkumarshee8@gmail.com>
2026-02-22 00:14:02 +0530

    add route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,40 +5,44 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import HomePage from "./pages/HomePage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
-import {Loader} from "lucide-react"
+import { Loader } from "lucide-react"
 import { Toaster } from "react-hot-toast";
 import { useThemeStore } from "./store/useThemeStore";
 const App = () => {
-  const{authUser,checkAuth,isCheckingAuth,onlineUsers}=useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
   const { theme } = useThemeStore();
 
-  useEffect(()=>{
-checkAuth();
-  },[checkAuth])
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
 
-  console.log("onlineUsers in App.jsx",onlineUsers);
-  console.log("authUser in App.jsx",authUser);
-  if(isCheckingAuth && !authUser){
-    return(
+  console.log("onlineUsers in App.jsx", onlineUsers);
+  console.log("authUser in App.jsx", authUser);
+  if (isCheckingAuth && !authUser) {
+    return (
       <div className="flex items-center justify-center h-screen">
 
-      <Loader className="size-10 animate-spin" />
+        <Loader className="size-10 animate-spin" />
       </div>
     )
   }
   return (
-    <div  data-theme={theme}>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
-        <Route path='/' element={ authUser ?<HomePage/>:<Navigate to="/login"/>} />
-        <Route path='/signup' element={ !authUser ? <SignUpPage/> : <Navigate to="/login"/>} />
-        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to="/"/>} />
-        <Route path='/Settings' element={<SettingsPage/>} />
-        <Route path='/Profile' element={authUser? <ProfilePage/> : <Navigate to="/login"/> } />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/forgot-password' element={!authUser ? <ForgotPasswordPage /> : <Navigate to="/" />} />
+        <Route path='/reset-password' element={!authUser ? <ResetPasswordPage /> : <Navigate to="/" />} />
+        <Route path='/Settings' element={<SettingsPage />} />
+        <Route path='/Profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 }
